a0b1a2d5a450b5f42bde7336ed227520f3579814
Azat Bertayev <azat.bertayev@gmail.com>
2024-01-17 10:38:59 +0600

    done

---
+++ TSIS1/data_types.py
@@ -0,0 +1,29 @@
+#1, int
+x = 5
+print(type(x))
+
+#2, str
+x = "Hello World"
+print(type(x))
+
+#3, float
+x = 20.5
+print(type(x))
+
+#4, list
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#5, tuple
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#6, dict
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#7, bool
+x = True
+print(type(x))
+
+
---
+++ TSIS1/variables.py
@@ -0,0 +1,22 @@
+#!
+carname = "Volvo"
+
+#2,3
+x = 50
+y = 10
+print(x+y)
+
+#4
+z = x+y
+print(z)
+
+#5
+a , b , c = "Orange", "Banana", "Cherry"
+
+#6
+q = w = r = "Orange"
+
+#7
+def myfunc():
+    global x
+    x = "fantastic"
