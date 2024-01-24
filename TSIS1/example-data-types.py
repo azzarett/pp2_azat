c57b74b347496e50db5458fb2e769ecf5e0542da
Azat Bertayev <azat.bertayev@gmail.com>
2024-01-24 12:22:57 +0600

    add example and some lab2 exercise

---
+++ TSIS1/example-data-types.py
@@ -0,0 +1,48 @@
+#1
+x = 5
+print(type(x))
+
+#2
+x = "Hello World"
+
+#3
+x = 20
+
+#4
+x = 20.5
+
+#5
+x = 1j
+
+#6
+x = ["apple", "banana", "cherry"]
+
+#7
+x = ("apple", "banana", "cherry")
+
+#8
+x = range(6)
+
+#9
+x = {"name" : "John", "age" : 36}
+
+#10
+x = {"apple", "banana", "cherry"}
+
+#11
+x = frozenset({"apple", "banana", "cherry"})
+
+#13
+x = True
+
+#14
+x = b"Hello"
+
+#15
+x = bytearray(5)
+
+#16
+x = memoryview(bytes(5))
+
+#17
+x = None
