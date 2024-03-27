6f5ad3c9779dc539b07da976b301c97b88058071
Azat Bertayev <azat.bertayev@gmail.com>
2024-03-27 11:29:50 +0500

    TSIS7

--- TSIS5/ex1.py
+++ TSIS5/ex1.py
@@ -1,10 +1,10 @@
 import re
 
-string1 = ["a", "ab", "abb", "bb", "aaa"]
+string = ["a", "ab", "abb", "bb", "aaa"]
 
 proverka = r'a+b*'
 
-for x in string1:
+for x in string:
     if re.fullmatch(proverka, x):
         print(f"{x} соответсвует")
     else:
